0cd6d9902f01efcb0ea0cb5ff7546fe140cc3f30
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-12-21 23:40:27 +0530

    changes

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -4,20 +4,24 @@ import img1 from "./images/tinder.png"
 import PersonIcon from '@mui/icons-material/Person';
 import ForumIcon from '@mui/icons-material/Forum';
 import IconButton from '@mui/material/IconButton';
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className="header">
-          <IconButton>
-            <PersonIcon className="header__icon" fontSize="large" />
-          </IconButton>
-          <img className="header__logo"  src={img1} 
-            alt="tinder logo"/>
-          <IconButton>
-            <ForumIcon className="header__icon " fontSize="large" />
-          </IconButton>
-        </div>
-    )
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+      <Link to="/">
+        <img className="header__logo" src={img1}
+          alt="tinder logo" />
+      </Link>
+      <Link to="/chat">
+        <IconButton>
+          <ForumIcon className="header__icon " fontSize="large" />
+        </IconButton>
+      </Link>
+    </div>
+  )
 }
 export default Header
- 
